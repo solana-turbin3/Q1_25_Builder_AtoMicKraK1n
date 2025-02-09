6588d0fdb2c3b052b3b50be452ea3b14ad88ca36
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-02-09 16:39:44 +0000

    errors optimized

--- amm/programs/amm/src/errors.rs
+++ amm/programs/amm/src/errors.rs
@@ -5,6 +5,7 @@
 use anchor_lang::error_code;
 use constant_product_curve::CurveError;
 
+
 #[error_code]
 pub enum AmmError {
     #[msg("DefaultError")]
@@ -17,18 +18,30 @@ pub enum AmmError {
     SlippageExceeded,
     #[msg("Invalid amount.")]
     InvalidAmount,
+    #[msg("Invalid precision.")]
+    InvalidPrecision,
+    #[msg("Overflow detected.")]
+    Overflow,
+    #[msg("Underflow detected.")]
+    Underflow,
+    #[msg("Invalid fee amount.")]
+    InvalidFeeAmount,
+    #[msg("Insufficient balance.")]
+    InsufficientBalance,
+    #[msg("Zero balance.")]
+    ZeroBalance,
 }
 
 impl From<CurveError> for AmmError {
     fn from(error: CurveError) -> AmmError {
         match error {
             CurveError::SlippageLimitExceeded => AmmError::SlippageExceeded,
-            CurveError::InvalidPrecision => todo!(),
-            CurveError::Overflow => todo!(),
-            CurveError::Underflow => todo!(),
-            CurveError::InvalidFeeAmount => todo!(),
-            CurveError::InsufficientBalance => todo!(),
-            CurveError::ZeroBalance => todo!(),
+            CurveError::InvalidPrecision => AmmError::InvalidPrecision,
+            CurveError::Overflow => AmmError::Overflow,
+            CurveError::Underflow => AmmError::Underflow,
+            CurveError::InvalidFeeAmount => AmmError::InvalidFeeAmount,
+            CurveError::InsufficientBalance => AmmError::InsufficientBalance,
+            CurveError::ZeroBalance => AmmError::ZeroBalance,
         }
     }
 }
